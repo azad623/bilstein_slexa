db95463f2b9e0a63b0b26dc661e245e045b0e445
azad623 <azad.abad@vanillasteel.com>
2024-11-01 11:03:02 +0100

    add docstrings to helper

--- bilstein_slexa/utils/helper.py
+++ bilstein_slexa/utils/helper.py
@@ -9,6 +9,12 @@ logger = logging.getLogger("<Bilstein SLExA ETL>")
 
 
 def save_pickle_file(df: pd.DataFrame, file_name: str) -> None:
+    """Save dataframe to pickle format
+
+    Args:
+        df (pd.DataFrame): Excel table converted to df
+        file_name (str): the name of expected pickle file
+    """
     try:
         with open(
             os.path.join(local_data_input_path, f"interim/{file_name}.pk"), "wb"
@@ -20,6 +26,14 @@ def save_pickle_file(df: pd.DataFrame, file_name: str) -> None:
 
 
 def load_pickle_file(file_name: str) -> pd.DataFrame:
+    """Load the pickle file inrto dataframe
+
+    Args:
+        file_name (str): pickle filename
+
+    Returns:
+        pd.DataFrame: Excel dataftame
+    """
     try:
         with open(
             os.path.join(local_data_input_path, "interim/{file_name}.pk"), "r"
@@ -31,7 +45,14 @@ def load_pickle_file(file_name: str) -> pd.DataFrame:
         logging.error(f"Could not load pickel file :{e}")
 
 
-def load_layout_schema(schema_path: str):
+def load_layout_schema(schema_path: str) -> dict | None:
+    """Load layout schema
+    Args:
+        schema_path (str): Path to the json schema
+
+    Returns:
+        dict : parsed json file
+    """
     try:
         with open(schema_path, "r") as f:
             schema = json.load(f)
